c908c3c2d9d8033d1a0da3ca6f15b3a8ef1fb508
Asmets <asmets@student.42.fr>
2014-11-26 02:41:21 +0100

    FINI OK OK

--- get_next_line.c
+++ get_next_line.c
@@ -1,55 +1,78 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   finalGNL.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: asmets <asmets@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2014/11/13 16:46:52 by asmets            #+#    #+#             */
-/*   Updated: 2014/11/18 01:28:07 by asmets           ###   ########.fr       */
+/*   Created: 2014/11/24 02:02:09 by asmets            #+#    #+#             */
+/*   Updated: 2014/11/26 02:40:56 by asmets           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <fcntl.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include "Libft/libft.h"
-#define BUFF_SIZE 10000
+#include "get_next_line.h"
 
+int		liberation(char **str, int size, int truc)
+{
+	char *tmp;
 
-// AJOUTER LES SECUS
-//
-int get_next_line(int const fd, char **line)
-{	
-	char *buff;
-	int ret;
-	char *str;
-	int i = 0;
-	int len = 0;
-
-	buff = ft_strnew(BUFF_SIZE);
-	ret = 0;
-	while ((ret = read(fd, buff, BUFF_SIZE) && (ft_strchr(buff, '\n'))))
+	if ((tmp = ft_strnew(size)) == NULL)
+		return (0);
+	if (*str)
+		tmp = ft_strcpy(tmp, *str);
+	if (*str && truc)
 	{
-		str = ft_strncpy(str, buff, (len =(ft_strchr(buff, '\n') - buff)));
-		buff = ft_strchr(buff, '\n') + 1;
+		free (*str);
+		*str = NULL;
 	}
-	while (i < len)
-	{
-		i++;
-	}
-	str[i] = '\0';
-	ft_putstr(str);
-	ft_putstr("LLLLOOOOLLLL\n");
-	return (0);
+	*str = tmp;
+	return (1);
 }
 
-int main()
+int		corps(char **buff, char **str, int truc)
 {
-	char *line = (char *)malloc(sizeof(char *)*  100);
-	int  fd; 	
+	int i[2];
 
-	fd = open("texte.txt", O_RDONLY);
-	get_next_line(fd, &line);
-	return (0);	
+	i[0] = 0;
+	i[1] = 0;
+	if (*str != NULL)
+		i[1] = ft_strlen(*str);
+	if (!(liberation(str, (i[1] + BUFF_SIZE), truc)))
+		return (-1);
+	while (buff[0][i[0]] != '\0')
+	{
+		if (buff[0][i[0]] == '\n')
+		{
+			str[0][i[1]] = '\0';
+			i[1] = 0;
+			while (buff[0][++i[0]] != '\0')
+				buff[0][i[1]++] = buff[0][i[0]];
+			buff[0][i[1]] = '\0';
+			return (RETURN_LINE);
+		}
+		str[0][i[1]++] = buff[0][i[0]++];
+	}
+	str[0][i[1]] = '\0';
+	return (2);
+}
+
+int		get_next_line(int const fd, char **line)
+{
+	static char *buff;
+	int			ret;
+	int			rread;
+
+	rread = 0;
+	if (buff != NULL && (ret = corps(&buff, line, 0)) == 1)
+		return (RETURN_LINE);
+	else if (ret == -1)
+		return (ret);
+	if (buff == NULL)
+		buff = ft_strnew(BUFF_SIZE);
+	while ((rread = read(fd, buff, BUFF_SIZE)) > 0)
+		if ((ret = corps(&buff, line, 1)) == 1)
+			return (RETURN_LINE);
+	if (rread == -1 || ret == -1)
+		return (-1);
+	return (0);
 }
